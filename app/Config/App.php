9e509089cb2c80e0b090f96bbf36b1a10f823929
Abel <j.abel.vera@gmail.com>
2022-05-19 13:34:54 -0500

    dia1

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://actividades-heroku.herokuapp.com/public/';
+    public $baseURL = 'public/';
 
     /**
      * --------------------------------------------------------------------------
